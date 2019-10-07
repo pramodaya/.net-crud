e3fa32d1a4e71d8830136143da5677ff3020f8c6
pramodaya <pramodayajayalath@gmail.com>
2019-10-07 23:36:40 +0530

    crud
    .net crud

---
+++ WebApplication1/WebApplication1/Controllers/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PlayerController : Controller
+    {
+        DataContext db = new DataContext();
+        // GET: Player
+        public ActionResult Index()
+        {
+            List<Player> player = db.player.ToList();
+            return View(player);
+        }
+
+        public ActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Player player)
+        {
+            db.player.Add(player);
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult Edit(int Id) {
+            Player player = db.player.Where(x => x.Id == Id).SingleOrDefault();
+            return View(player);
+        }
+
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Player> player { get; set; }
+    }
+}
